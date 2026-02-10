4aa490b5ee78ac3df6e90bd7c834cc2343f7ec72
Pranav Nikam <pranavnikam15pn@gmail.com>
2026-02-10 23:35:44 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (75.94%) - LeetSync

---
+++ C/69-sqrtx/sqrtx.c
@@ -0,0 +1,44 @@
+// Binary Search Approch
+
+int mySqrt(int x) {
+    if(x < 2){
+        return x;
+    }
+
+    int start = 1, end = x/2;
+    int ans = 0;
+    while(start <= end){
+        long mid = start + (end - start)/2;
+        if(mid*mid <= x){
+            ans = mid;
+            start = mid+1;
+        } else {
+            end = mid-1;
+        }
+    }
+    return ans;
+}
+
+
+/*
+    // Linear Method
+
+    int mySqrt(int x) {
+        if (x < 2){
+            return x;
+        } 
+
+        int start = 1, end = x/2;
+        int ans = 0;
+        for(long i=start; i<=end; i++){
+            if(i*i <= x){
+                ans=i;
+            } else {
+                break;
+            }
+        }
+
+        return ans;
+    }
+
+*/
