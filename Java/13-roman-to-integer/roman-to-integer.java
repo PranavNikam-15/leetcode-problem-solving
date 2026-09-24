79f9d597e24b40591eb8aeacdf611a7c56f25849
Pranav Nikam <pranavnikam15pn@gmail.com>
2026-09-24 20:26:38 +0530

    Time: 3 ms (76.29%) | Memory: 46.4 MB (60.77%) - LeetSync

---
+++ Java/13-roman-to-integer/roman-to-integer.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int romanToInt(String s) {
+        
+        int result = 0;
+        int n = s.length();
+
+        for(int i=0; i<n; i++) {
+            
+            int current = getIntegerValue(s.charAt(i));
+            
+            if((i+1) < n && current < getIntegerValue(s.charAt(i + 1))) {
+                result -= current;
+            } else {
+                result += current;
+            }
+        } 
+
+        return result;
+    }
+
+    public int getIntegerValue(char ch) {
+
+        switch(ch) {
+            
+            case 'I' : return 1;
+            case 'V' : return 5;
+            case 'X' : return 10;
+            case 'L' : return 50;
+            case 'C' : return 100;
+            case 'D' : return 500;
+            case 'M' : return 1000;
+
+            default : return 0; 
+        }
+    }
+}
